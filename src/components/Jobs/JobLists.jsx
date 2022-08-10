ac030f86af3bb5f76b1a62e92cde9ee8a828c246
kuchkorov <kuchkorov.mamurjon@mail.ru>
2022-08-10 11:56:52 +0500

    JobLists page, filter jobData changed

--- src/components/Jobs/JobLists.jsx
+++ src/components/Jobs/JobLists.jsx
@@ -22,70 +22,79 @@ const JobLists = () => {
   };
 
   // ======== Filter data by part-time, full-time, freelance etc======
-  const filterJobData = (e) => {
+  const filterJobData = (e, type) => {
     const filterValue = e.target.value;
 
-    if (filterValue === "full-time") {
-      const filteredData = jobs.filter((job) => job.contract === "Full Time");
-      setJobData(filteredData);
-    } else if (filterValue === "part-time") {
-      const filteredData = jobs.filter((job) => job.contract === "Part Time");
-      setJobData(filteredData);
-    } else if (filterValue === "freelance") {
-      const filteredData = jobs.filter((job) => job.contract === "Freelance");
-      setJobData(filteredData);
-    } else if (filterValue === "contract") {
-      const filteredData = jobs.filter((job) => job.contract === "Contract");
-      setJobData(filteredData);
-    } else {
-      setJobData(jobs);
-    }
+    setJobData(
+      jobData?.filter((item) => {
+        if (type === "search") {
+          return item?.contract?.includes(filterValue);
+        } else {
+          return item?.location?.includes(filterValue);
+        }
+      })
+    );
+    // if (filterValue === "full-time") {
+    //   const filteredData = jobs.filter((job) => job.contract === "Full Time");
+    //   setJobData(filteredData);
+    // } else if (filterValue === "part-time") {
+    //   const filteredData = jobs.filter((job) => job.contract === "Part Time");
+    //   setJobData(filteredData);
+    // } else if (filterValue === "freelance") {
+    //   const filteredData = jobs.filter((job) => job.contract === "Freelance");
+    //   setJobData(filteredData);
+    // } else if (filterValue === "contract") {
+    //   const filteredData = jobs.filter((job) => job.contract === "Contract");
+    //   setJobData(filteredData);
+    // } else {
+    //   setJobData(jobs);
+    // }
   };
 
   return (
-    <section className="job__list">
-      <div className="container">
-        <div className="job__list__wrapper">
-          <div className="search__panel">
-            <div className="search__panel-01">
+    <section className='job__list'>
+      <div className='container'>
+        <div className='job__list__wrapper'>
+          <div className='search__panel'>
+            <div className='search__panel-01'>
               <span>
-                <i class="ri-search-line"></i>
+                <i class='ri-search-line'></i>
               </span>
               <input
-                type="text"
-                placeholder="Search by title, companies"
+                type='text'
+                placeholder='Search by title, companies'
                 value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
+                onChange={(e) => filterJobData(e, "search")}
               />
             </div>
 
-            <div className="search__panel-02">
+            <div className='search__panel-02'>
               <span>
-                <i class="ri-map-pin-line"></i>
+                <i class='ri-map-pin-line'></i>
               </span>
               <input
-                type="text"
-                placeholder="Search by location"
+                type='text'
+                placeholder='Search by location'
                 value={searchByLocation}
-                onChange={(e) => setSearchByLocation(e.target.value)}
+                onChange={(e) => filterJobData(e, "location")}
               />
-              <button className="btn" onClick={locationSearchHandler}>
+              <button className='btn' onClick={locationSearchHandler}>
                 Search
               </button>
             </div>
 
-            <div className="search__panel-03">
-              <select onChange={filterJobData}>
+            <div className='search__panel-03'>
+              <select onChange={() => filterJobData(e, "search")}>
                 <option>Filter job by</option>
-                <option value="full-time">Full Time</option>
-                <option value="part-time">Part Time</option>
-                <option value="freelance">Freelance</option>
-                <option value="contract">Contract</option>
+                <option value='full-time'>Full Time</option>
+                <option value='part-time'>Part Time</option>
+                <option value='freelance'>Freelance</option>
+                <option value='contract'>Contract</option>
               </select>
             </div>
           </div>
 
-          <div className="jobs__wrapper">
+          <div className='jobs__wrapper'>
             {jobData
               ?.filter((job) => {
                 if (searchTerm === "") return job;
@@ -96,10 +105,10 @@ const JobLists = () => {
                   return job;
               })
               .map((item) => (
-                <div className="job__item" key={item.id}>
-                  <img src={item.logo} alt="" />
+                <div className='job__item' key={item.id}>
+                  <img src={item.logo} alt='' />
 
-                  <div className="job__content">
+                  <div className='job__content'>
                     <h6>
                       {item.postedAt} - {item.contract}
                     </h6>
@@ -108,7 +117,7 @@ const JobLists = () => {
                     </h1>
                     <p>{item.company}</p>
 
-                    <div className="location">
+                    <div className='location'>
                       <p>
                         Location: <span>{item.location}</span>
                       </p>
